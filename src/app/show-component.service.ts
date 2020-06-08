182c861c56c6c611fef03322af2e4f5f91c5d4b9
Micharzu <Micharzuc@gmail.com>
2020-06-08 22:20:44 +0200

    add animation to skills

--- src/app/show-component.service.ts
+++ src/app/show-component.service.ts
@@ -9,18 +9,28 @@ export class ShowComponentService{
     private showAboutMe = new BehaviorSubject<boolean>(false);
     aboutMeState = this.showAboutMe.asObservable();
 
-    readonly observerOptions = {};
+    private showSkills = new BehaviorSubject<boolean>(false);
+    skillsState = this.showSkills.asObservable();
+
+    readonly observerOptions = {
+        threshold: 0.6
+    };
 
     readonly observer = new IntersectionObserver((entries, observer)=>{
         entries.forEach(entry=>{
             if(entry.isIntersecting){
+                if(!entry.target.classList.contains('initiated')){
+                    entry.target.classList.add('initiated');
+                }
                 entry.target.classList.add('is-visible');
+                
                 this.setComponent(entry.target.id, true);
                 if(entry.target.classList.contains('keep-visible')){
                     observer.unobserve(entry.target);
                 }
             } else if(entry.target.classList.contains('is-visible')){
                 entry.target.classList.remove('is-visible');
+                entry.target.classList.add('scrolled');
                 this.setComponent(entry.target.id, false);
             }
         });
@@ -30,6 +40,10 @@ export class ShowComponentService{
         switch(sectionId){
             case "about-me-section":{
                 this.showAboutMe.next(newState);
+                break;
+            }
+            case "skills-section":{
+                this.showSkills.next(newState);
                 break;
             }
         }
--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ShowComponentService } from '../show-component.service';
 
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.css']
+  styleUrls: ['./styles/skills.component.css']
 })
-export class SkillsComponent implements OnInit {
+export class SkillsComponent implements OnInit, OnDestroy {
+  private subscription: Subscription;
+  rootElement: Element;
 
-  constructor() { }
+  constructor(private showCompService: ShowComponentService) { }
 
   ngOnInit() {
+    this.rootElement = document.getElementsByClassName('skills')[0];
+
+    this.subscription = this.showCompService.skillsState.subscribe(state =>{
+      if(state){
+        this.rootElement.classList.add('is-visible');
+        if(!this.rootElement.classList.contains('initiated')){
+          this.rootElement.classList.add('initiation');
+          this.rootElement.classList.add('initiated');
+          setTimeout(()=>{this.rootElement.classList.remove('initiation')}, 10000);
+        }
+      } else{
+        if(this.rootElement.classList.contains('is-visible')){
+          this.rootElement.classList.remove('is-visible');
+        }
+        
+      }
+    });
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
   }
 
 }
