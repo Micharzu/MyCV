5bd5a0343819ac10630e442fc42d6a7ba71b8cd0
Micharzu <micharzuc@gmail.com>
2020-10-13 21:52:11 +0200

    prevent skill animation from increasing page size

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,40 +1,43 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { ShowComponentService } from '../show-component.service';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Subscription } from "rxjs";
+import { ShowComponentService } from "../show-component.service";
 
 @Component({
-  selector: 'app-skills',
-  templateUrl: './skills.component.html',
-  styleUrls: ['./styles/skills.component.css']
+  selector: "app-skills",
+  templateUrl: "./skills.component.html",
+  styleUrls: ["./styles/skills.component.css"],
 })
 export class SkillsComponent implements OnInit, OnDestroy {
   private subscription: Subscription;
   rootElement: Element;
+  childElement: Element;
 
-  constructor(private showCompService: ShowComponentService) { }
+  constructor(private showCompService: ShowComponentService) {}
 
   ngOnInit() {
-    this.rootElement = document.getElementsByClassName('skills')[0];
+    this.rootElement = document.getElementsByClassName("skills")[0];
+    this.childElement = document.getElementsByClassName("skill_list")[0];
 
-    this.subscription = this.showCompService.skillsState.subscribe(state =>{
-      if(state){
-        this.rootElement.classList.add('is-visible');
-        if(!this.rootElement.classList.contains('initiated')){
-          this.rootElement.classList.add('initiation');
-          this.rootElement.classList.add('initiated');
-          setTimeout(()=>{this.rootElement.classList.remove('initiation')}, 10000);
+    this.subscription = this.showCompService.skillsState.subscribe((state) => {
+      if (state) {
+        this.rootElement.classList.add("is-visible");
+        if (!this.rootElement.classList.contains("initiated")) {
+          this.rootElement.classList.add("initiation");
+          this.rootElement.classList.add("initiated");
+          this.childElement.classList.add("initiated");
+          setTimeout(() => {
+            this.rootElement.classList.remove("initiation");
+          }, 10000);
         }
-      } else{
-        if(this.rootElement.classList.contains('is-visible')){
-          this.rootElement.classList.remove('is-visible');
+      } else {
+        if (this.rootElement.classList.contains("is-visible")) {
+          this.rootElement.classList.remove("is-visible");
         }
-        
       }
     });
   }
 
-  ngOnDestroy(){
+  ngOnDestroy() {
     this.subscription.unsubscribe();
   }
-
 }
