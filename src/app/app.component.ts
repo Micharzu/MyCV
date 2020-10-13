01592197d59016174023b0dba773418ee44ae966
Micharzu <micharzuc@gmail.com>
2020-10-13 17:46:12 +0200

    fix sliding up subs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,22 @@
-import { Component, OnInit, OnDestroy} from '@angular/core';
-import { ShowComponentService } from './show-component.service';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { ShowComponentService } from "./show-component.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./styles/app-component.css"],
 })
-export class AppComponent implements OnInit, OnDestroy{
-  title = 'MyCV';
+export class AppComponent implements OnInit, OnDestroy {
+  title = "MyCV";
 
-  constructor(private showCompService: ShowComponentService){}
+  constructor(private showCompService: ShowComponentService) {}
 
-  ngOnInit(){
+  ngOnInit() {
     this.showCompService.sections = document.querySelectorAll("section");
-    this.showCompService.sections.forEach(section=>{
+    this.showCompService.sections.forEach((section) => {
       this.showCompService.observer.observe(section);
     });
   }
 
-  ngOnDestroy(){
-  }
-  
+  ngOnDestroy() {}
 }
