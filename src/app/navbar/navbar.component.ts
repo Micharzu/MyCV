403d6036e41c959f12d564ed69048e9ca22ab410
Micharzu <micharzuc@gmail.com>
2020-10-16 18:18:48 +0200

    add responsiveness to navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,38 +1,73 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./styles/navbar.component.css"],
 })
 export class NavbarComponent implements OnInit {
+  constructor() {}
+  mainContent: HTMLElement;
+  navbar: HTMLElement;
+  navSlider: HTMLElement;
+  navLinks: NodeListOf<HTMLElement>;
 
-  constructor() { }
-
-  toHeader(){
-    document.getElementById("header-section").scrollIntoView({behavior: "smooth"});
+  toHeader() {
+    document
+      .getElementById("header-section")
+      .scrollIntoView({ behavior: "smooth" });
   }
 
-  toAboutMe(){
-    document.getElementById("about-me-section").scrollIntoView({behavior: "smooth"});
+  toAboutMe() {
+    document
+      .getElementById("about-me-section")
+      .scrollIntoView({ behavior: "smooth" });
   }
 
-  toSkills(){
-    document.getElementById("skills-section").scrollIntoView({behavior: "smooth"});
+  toSkills() {
+    document
+      .getElementById("skills-section")
+      .scrollIntoView({ behavior: "smooth" });
   }
 
-  toHobbies(){
-    document.getElementById("hobbies-section").scrollIntoView({behavior: "smooth"});
+  toHobbies() {
+    document
+      .getElementById("hobbies-section")
+      .scrollIntoView({ behavior: "smooth" });
   }
 
-  toContact(){
-    document.getElementById("contact-section").scrollIntoView({behavior: "smooth"});
+  toContact() {
+    document
+      .getElementById("contact-section")
+      .scrollIntoView({ behavior: "smooth" });
   }
 
-  toSubmissions(){
-    document.getElementById("submissions-section").scrollIntoView({behavior: "smooth"});
+  toSubmissions() {
+    document
+      .getElementById("submissions-section")
+      .scrollIntoView({ behavior: "smooth" });
   }
 
-  ngOnInit(){
+  toggleNav() {
+    this.navbar.classList.toggle("nav-active");
+    this.navSlider.classList.toggle("nav-active");
+    this.mainContent.classList.toggle("nav-active");
+
+    this.navLinks.forEach((link, index) => {
+      if (link.style.animation.includes("navLinkFadeIn")) {
+        link.style.animation = "navLinkFadeOut 0.5s ease forwards";
+      } else {
+        link.style.animation = `navLinkFadeIn 0.5s ease forwards ${
+          index / 7 + 0.3
+        }s`;
+      }
+    });
+  }
+
+  ngOnInit() {
+    this.mainContent = document.querySelector(".main");
+    this.navbar = document.querySelector(".navbar");
+    this.navSlider = document.querySelector(".nav-links");
+    this.navLinks = document.querySelectorAll(".nav-links li");
   }
 }
